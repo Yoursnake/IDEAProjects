7c8e2253b82068945ee3b7121b3eaa14828ec20d
Yoursnake <893077810@qq.com>
2017-03-18 17:09:00 +0800

    leetcode5longestPalindrome

--- arithmetic/src/Main.java
+++ arithmetic/src/Main.java
@@ -7,13 +7,55 @@ import java.util.Scanner;
  */
 public class Main {
 
-    static int i = 0;
+    static int maxLength;
+    static String res;
 
     public static void main(String[] args) {
-        String s = "MCMXCVI";
-        System.out.println(leetcode13RomanToInt.romanToInt(s));
+        String s = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabcaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+//        String s = "aaaabbbbbbbbbbccccccccccdddddadddddeeeeeeeeeeffffffffffgggggggggghhhhhhhhhhiiiiiiiiiijjjjjjjjjjkkkkkkkkkkllllllllllmmmmmmmmmmnnnnnnnnnnooooooooooppppppppppqqqqqqqqqqrrrrrrrrrrssssssssssttttttttttuuuuuuuuuuvvvvvvvvvvwwwwwwwwwwxxxxxxxxxxyyyyyyyyyyzzzzzzzzzzyyyyyyyyyyxxxxxxxxxxwwwwwwwwwwvvvvvvvvvvuuuuuuuuuuttttttttttssssssssssrrrrrrrrrrqqqqqqqqqqppppppppppoooooooooonnnnnnnnnnmmmmmmmmmmllllllllllkkkkkkkkkkjjjjjjjjjjiiiiiiiiiihhhhhhhhhhggggggggggffffffffffeeeeeeeeeeddddddddddccccccccccbbbbbbbbbbaaaaaaaabbbbbbbbbbccccccccccddddddddddeeeeeeeeeeffffffffffgggggggggghhhhhhhhhhiiiiiiiiiijjjjjjjjjjkkkkkkkkkkllllllllllmmmmmmmmmmnnnnnnnnnnooooooooooppppppppppqqqqqqqqqqrrrrrrrrrrssssssssssttttttttttuuuuuuuuuuvvvvvvvvvvwwwwwwwwwwxxxxxxxxxxyyyyyyyyyyzzzzzzzzzzyyyyyyyyyyxxxxxxxxxxwwwwwwwwwwvvvvvvvvvvuuuuuuuuuuttttttttttssssssssssrrrrrrrrrrqqqqqqqqqqppppppppppoooooooooonnnnnnnnnnmmmmmmmmmmllllllllllkkkkkkkkkkjjjjjjjjjjiiiiiiiiiihhhhhhhhhhggggggggggffffffffffeeeeeeeeeeddddddddddccccccccccbbbbbbbbbbaaaa";
+//        String s = "ccc";
+        System.out.println(longestPalindrome(s));
 
     }
 
+    public static String longestPalindrome(String s) {
+        for (int i = 0; i < s.length(); i++) {
+            if (i < s.length() - 1) {
+                if (s.charAt(i) == s.charAt(i+1)) {
+                    Judge(s,i);
+                    Judge(s, i ,i+1);
+                } else {
+                    Judge(s, i);
+                }
+            }
 
+        }
+        return res;
+    }
+
+    public static void Judge(String s, int i) {
+        int left = i, right = i;
+        while (s.charAt(left) == s.charAt(right)) {
+            right++;
+            left--;
+            if (right >= s.length() || left < 0) break;
+        }
+        if (right - left - 1 > maxLength) {
+            maxLength = right - left - 1;
+            res = s.substring(left+1, right);
+        }
+    }
+
+    public static void Judge(String s, int i, int j) {
+        int left = i, right = j;
+        while (s.charAt(left) == s.charAt(right)) {
+            right++;
+            left--;
+            if (right >= s.length() || left < 0) break;
+        }
+        if (right - left - 1 > maxLength) {
+            maxLength = right - left - 1;
+            res = s.substring(left+1, right);
+        }
+    }
 }
---
+++ arithmetic/src/leetcode5longestPalindrome.java
@@ -0,0 +1,53 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by shengliyi on 2017/3/18.
+ */
+public class leetcode5longestPalindrome {
+
+    static int maxLength;
+    static String res;
+
+    public static String longestPalindrome(String s) {
+        for (int i = 0; i < s.length(); i++) {
+            if (i < s.length() - 1) {
+                if (s.charAt(i) == s.charAt(i+1)) {
+                    Judge(s,i);
+                    Judge(s, i ,i+1);
+                } else {
+                    Judge(s, i);
+                }
+            }
+
+        }
+        return res;
+    }
+
+    public static void Judge(String s, int i) {
+        int left = i, right = i;
+        while (s.charAt(left) == s.charAt(right)) {
+            right++;
+            left--;
+            if (right >= s.length() || left < 0) break;
+        }
+        if (right - left - 1 > maxLength) {
+            maxLength = right - left - 1;
+            res = s.substring(left+1, right);
+        }
+    }
+
+    public static void Judge(String s, int i, int j) {
+        int left = i, right = j;
+        while (s.charAt(left) == s.charAt(right)) {
+            right++;
+            left--;
+            if (right >= s.length() || left < 0) break;
+        }
+        if (right - left - 1 > maxLength) {
+            maxLength = right - left - 1;
+            res = s.substring(left+1, right);
+        }
+    }
+}
