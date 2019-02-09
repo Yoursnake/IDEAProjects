205ebf94fb8a29a4ee6ed18b43913e5e6dd410b0
lonely7yk <893077810@qq.com>
2019-02-09 22:15:47 +0800

    LeetCode60

--- arithmetic/src/LeetCode60PermutationSequence.java
+++ arithmetic/src/LeetCode60PermutationSequence.java
@@ -1,2 +1,92 @@
+/*
+Example 1:
+
+Input: n = 3, k = 3
+Output: "213"
+
+Example 2:
+
+Input: n = 4, k = 9
+Output: "2314"
+ */
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class LeetCode60PermutationSequence {
+//    // 方法1 : 回溯法找出所有字符串，然后找第 k 个
+//    public String getPermutation(int n, int k) {
+//        List<Character> nums = new ArrayList<>();
+//        List<String> permutations = new ArrayList<>();
+//        String curr = "";
+//
+//        for (int i = 0; i < n; i++) {
+//            nums.add((char)(i + '1'));
+//        }
+//
+//        getAllPermutations(permutations, curr, nums);
+//        return permutations.get(k - 1);
+//    }
+//
+//    void getAllPermutations(List<String> permutations, String curr, List<Character> nums) {
+//        if (nums.size() == 0) {
+//            permutations.add(curr);
+//            return;
+//        }
+//
+//        for (int i = 0; i < nums.size(); i++) {
+//            String tempCurr = curr + nums.get(i);
+//            List<Character> tempNums = new ArrayList<>(nums);
+//            tempNums.remove(i);
+//            getAllPermutations(permutations, tempCurr, tempNums);
+//        }
+//    }
+
+    // 方法2 : 使用递归求解，每次算当前剩余数字中的首位变化一位需要的数，然后求倍数，
+    // 这样当前首位就是剩余数字中索引为倍数的数（注意能整除的情况就行）
+    public String getPermutation(int n, int k) {
+        String result = new String("");
+        List<Character> nums = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            nums.add((char)(i + '1'));
+        }
+
+        result = getPermutation(nums, k);
+
+        return result;
+    }
+
+    String getPermutation(List<Character> nums, int remainder) {
+        int numsLen = nums.size();  // 即 n
+        if (numsLen == 1) {
+            return nums.get(0) + "";
+        }
+
+        int divisor = factorial(numsLen - 1);
+        int multiple = remainder / divisor;
+
+        // 当能整除时就放出一位，作为余数
+        if (remainder % divisor == 0) {
+            multiple--;
+            remainder = divisor;
+        } else {
+            remainder = remainder % divisor;
+        }
+
+        char addChar = nums.get(multiple);
+        nums.remove(multiple);
+        return addChar + getPermutation(nums, remainder);
+    }
+
+    // 阶乘
+    int factorial(int n) {
+        int result = 1;
+
+        for (int i = 2; i <= n; i++) {
+            result *= i;
+        }
+
+        return result;
+    }
 }
--- arithmetic/src/Main.java
+++ arithmetic/src/Main.java
@@ -1,6 +1,3 @@
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * Created by shengliyi on 2017/3/4.
  */
@@ -10,19 +7,7 @@ public class Main {
     public static void main(String[] args) {
 //        boolean[][] isUsed = new boolean[2][3];
 
-
-        List<Interval> intervals = new ArrayList<>();
-        intervals.add(new Interval(1, 10));
-        intervals.add(new Interval(2, 3));
-        intervals.add(new Interval(4, 5));
-        intervals.add(new Interval(6, 7));
-        intervals.add(new Interval(8, 9));
-
-
-        List<Interval> list = new LeetCode56MergeIntervals().merge(intervals);
-
-        for (int i = 0; i < list.size(); i++) {
-            System.out.println(list.get(i).start + "\t" + list.get(i).end);
-        }
+        String result = new LeetCode60PermutationSequence().getPermutation(4, 9);
+        System.out.println(result);
     }
 }
