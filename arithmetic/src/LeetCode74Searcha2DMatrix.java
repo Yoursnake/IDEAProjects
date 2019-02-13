a0aa239015a8f0ac8b7fc4be977ce45c9e107316
lonely7yk <893077810@qq.com>
2019-02-13 16:28:08 +0800

    LeetCode74

---
+++ arithmetic/src/LeetCode74Searcha2DMatrix.java
@@ -0,0 +1,62 @@
+/*
+Example 1:
+
+Input:
+matrix = [
+  [1,   3,  5,  7],
+  [10, 11, 16, 20],
+  [23, 30, 34, 50]
+]
+target = 3
+Output: true
+
+Example 2:
+
+Input:
+matrix = [
+  [1,   3,  5,  7],
+  [10, 11, 16, 20],
+  [23, 30, 34, 50]
+]
+target = 13
+Output: false
+ */
+public class LeetCode74Searcha2DMatrix {
+    public boolean searchMatrix(int[][] matrix, int target) {
+
+        int row = matrix.length;
+        if (row == 0) return false;
+        int col = matrix[0].length;
+        if (col == 0) return false;
+        if (target < matrix[0][0] || target > matrix[row - 1][col - 1]) return false;
+
+        int targetRow = -1;
+
+        // 找target所在的行
+        if (row == 1) {
+            targetRow = 0;
+        } else {
+            for (int i = 0; i < row - 1; i++) {
+                if (matrix[i][0] == target) return true;
+                if (matrix[i][0] < target && target < matrix[i + 1][0]) {
+                    targetRow = i;
+                }
+            }
+            if (targetRow == -1) targetRow = row - 1;
+        }
+
+        // 二分法找元素
+        int low = 0, high = col - 1;
+        while (true) {
+            int mid = (low + high) / 2;
+            if (matrix[targetRow][mid] == target) return true;
+            else if (matrix[targetRow][mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+
+            if (low > high) return false;
+        }
+    }
+}
--- arithmetic/src/Main.java
+++ arithmetic/src/Main.java
@@ -6,8 +6,10 @@ public class Main {
 //-2147483648
     public static void main(String[] args) {
 //        boolean[][] isUsed = new boolean[2][3];
+        int[][] a = {{1, 3, 5, 7}, {10, 11, 16, 20}, {23, 30, 34, 50}};
+        boolean b = new LeetCode74Searcha2DMatrix().searchMatrix(a, 24);
+        System.out.println(b);
 
-
-    }
+}
 
 }
