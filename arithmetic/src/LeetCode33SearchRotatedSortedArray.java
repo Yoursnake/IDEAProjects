ed1fb9718dd889e6ce28c5514b88bc1c880ac29d
lonely7yk <893077810@qq.com>
2019-02-17 19:08:09 +0800

    LeetCode 33 update

--- arithmetic/src/LeetCode33SearchRotatedSortedArray.java
+++ arithmetic/src/LeetCode33SearchRotatedSortedArray.java
@@ -1,3 +1,14 @@
+/*
+Example 1:
+
+Input: nums = [4,5,6,7,0,1,2], target = 0
+Output: 4
+
+Example 2:
+
+Input: nums = [4,5,6,7,0,1,2], target = 3
+Output: -1
+ */
 public class LeetCode33SearchRotatedSortedArray {
     public int search(int[] nums, int target) {
 
