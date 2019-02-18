c3fe2eebba2299750748ee3c796bd3b0dce4138e
lonely7yk <893077810@qq.com>
2019-02-18 21:43:10 +0800

    LeetCode 82

---
+++ arithmetic/src/LeetCode82RemoveDuplicatesfromSortedListII.java
@@ -0,0 +1,65 @@
+/*
+Example 1:
+
+Input: 1->2->3->3->4->4->5
+Output: 1->2->5
+
+Example 2:
+
+Input: 1->1->1->2->3
+Output: 2->3
+ */
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class LeetCode82RemoveDuplicatesfromSortedListII {
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null || head.next == null) return head; // 只有一个结点的情况
+
+        // 先把开头的重复元素去除
+        while (head.val == head.next.val) {
+            ListNode newHead = head;
+            while (newHead.val == newHead.next.val) {
+                newHead = newHead.next;
+                if (newHead.next == null) return null;  // 保证 newHead.next 不为空
+            }
+            head = newHead.next;
+            if (head.next == null) return head; // 保证 head.next 不为空
+        }
+
+        if (head.next == null || head.next.next == null) return head; // 如果这个时候只有一个结点
+
+        ListNode headNode = head;
+        ListNode afterNode = head.next;
+        boolean flag = false;       // 表示是否连续
+
+        while (afterNode.next != null) {
+            if (afterNode.val == afterNode.next.val) {
+                flag = true;
+                afterNode = afterNode.next;
+                // 如果 afterNode.next 是 null，证明最后几个数都是一致的
+                if (afterNode.next == null) {
+                    headNode.next = null;
+                    break;
+                }
+            } else {
+                if (!flag) {    // 不连续的话直接往后走就行了
+                    headNode = headNode.next;
+                    afterNode = afterNode.next;
+                } else {
+                    headNode.next = afterNode.next;
+                    afterNode = afterNode.next;
+                    flag = false;
+                }
+            }
+        }
+
+        return head;
+    }
+}
--- arithmetic/src/Main.java
+++ arithmetic/src/Main.java
@@ -5,19 +5,29 @@
 public class Main {
 //-2147483648
     public static void main(String[] args) {
-        boolean[][] isUsed = new boolean[2][3];
-//        char[][] board = {
-//                {'A', 'B', 'C', 'E'},
-//                {'S', 'F', 'C', 'S'},
-//                {'A', 'D', 'E', 'E'}};
-        int[] nums = {3,1};
-        int target = 1;
-//        int[] nums = {0, 0, 1, 1, 1, 1, 2, 3, 3};
-        int result = new LeetCode33SearchRotatedSortedArray().search(nums, target);
-        System.out.println(result);
+        int[] nums = {1, 1,2};
+        ListNode head = new ListNode(nums[0]);
+        ListNode a = head;
 
+        for (int i = 1; i < nums.length; i++) {
+            a.next = new ListNode(nums[i]);
+            a = a.next;
+        }
 
+        a = head;
+        while (a != null) {
+            System.out.print(a.val + "\t");
+            a = a.next;
+        }
+        System.out.println();
 
+        ListNode result = new LeetCode82RemoveDuplicatesfromSortedListII().deleteDuplicates(head);
+
+        a = result;
+        while (a != null) {
+            System.out.print(a.val + "\t");
+            a = a.next;
+        }
     }
 
 }
