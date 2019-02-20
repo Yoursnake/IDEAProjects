4e44b385b4e9186c4fe8befd4a644deb230d6701
lonely7yk <893077810@qq.com>
2019-02-20 11:42:15 +0800

    LeetCode56 turn Interval to new class

--- algorithm/src/Interval.java
+++ algorithm/src/Interval.java
@@ -1,4 +1,7 @@
-package PACKAGE_NAME;
-
 public class Interval {
+    int start;
+    int end;
+    Interval() { start = 0; end = 0; }
+    Interval(int s, int e) { start = s; end = e; }
+
 }
--- algorithm/src/LeetCode56MergeIntervals.java
+++ algorithm/src/LeetCode56MergeIntervals.java
@@ -57,13 +57,7 @@ public class LeetCode56MergeIntervals {
 
 }
 
-class Interval {
-    int start;
-    int end;
-    Interval() { start = 0; end = 0; }
-    Interval(int s, int e) { start = s; end = e; }
 
-}
 
 
 
