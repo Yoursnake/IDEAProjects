39b52af0bec39e09d2417ac48391f6d15053cec5
lonely7yk <893077810@qq.com>
2019-07-05 22:30:41 +0800

    LeetCode 144

--- algorithm/src/Main.java
+++ algorithm/src/Main.java
@@ -1,21 +1,16 @@
+import java.util.List;
+
 /**
  * Created by shengliyi on 2017/3/4.
  */
 
 public class Main {
     public static void main(String[] args) {
+        LeetCode144BinaryTreePreorderTraversal.TreeNode root = new LeetCode144BinaryTreePreorderTraversal.TreeNode(1);
+        root.right = new LeetCode144BinaryTreePreorderTraversal.TreeNode(2);
+        root.right.left = new LeetCode144BinaryTreePreorderTraversal.TreeNode(3);
 
-        LeetCode143ReorderList.ListNode node = new LeetCode143ReorderList.ListNode(1);
-        node.next = new LeetCode143ReorderList.ListNode(2);
-        node.next.next = new LeetCode143ReorderList.ListNode(3);
-        node.next.next.next = new LeetCode143ReorderList.ListNode(4);
-        node.next.next.next.next = new LeetCode143ReorderList.ListNode(5);
-
-        new LeetCode143ReorderList().reorderList(node);
-
-        while (node != null) {
-            System.out.println(node.val);
-            node = node.next;
-        }
+        List<Integer> result = new LeetCode144BinaryTreePreorderTraversal().preorderTraversal(root);
+        System.out.println(result);
     }
 }
