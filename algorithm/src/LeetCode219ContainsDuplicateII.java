d98912e5cfedd0f18d0b5d0424b2cfdcaec65fd7
lonely7yk <893077810@qq.com>
2019-09-16 22:09:14 +0800

    Leet219 HashMap

--- algorithm/src/LeetCode219ContainsDuplicateII.java
+++ algorithm/src/LeetCode219ContainsDuplicateII.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
+import java.util.HashMap;
+import java.util.Map;
 
 public class LeetCode219ContainsDuplicateII {
+    // HashMap: 81.34%
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(nums[i]) && (i - map.get(nums[i]) <= k)) return true;
+            map.put(nums[i], i);
+        }
+
+        return false;
+    }
 }
