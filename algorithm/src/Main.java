82ac37a4bdf45f3ac39cb42f3823949bfc230d55
lonely7yk <893077810@qq.com>
2019-12-07 22:34:33 +0800

    use junit to test

--- algorithm/src/Main.java
+++ algorithm/src/Main.java
@@ -1,4 +1,4 @@
-import java.util.List;
+import org.junit.Test;
 
 /**
  * Created by shengliyi on 2017/3/4.
@@ -6,35 +6,8 @@ import java.util.List;
 
 public class Main {
 
-	public static class NInteger implements NestedInteger {
-
-		private Integer val;
-		private List<NestedInteger> list;
-
-		public NInteger(Integer val, List<NestedInteger> list) {
-			this.val = val;
-			this.list = list;
-		}
-
-		@Override
-		public boolean isInteger() {
-			return this.val != null;
-		}
-
-		@Override
-		public Integer getInteger() {
-			if (isInteger()) return this.val;
-			else return null;
-		}
-
-		@Override
-		public List<NestedInteger> getList() {
-			if (!isInteger()) return this.list;
-			else return null;
-		}
-	}
-
-    public static void main(String[] args) {
+    @Test
+	public void LeetCode377() {
 	    int[] nums = {2, 1, 3};
 	    int target = 35;
 
@@ -42,25 +15,4 @@ public class Main {
 	    System.out.println(res);
     }
 
-    public static void printLinkedList(LeetCode206ReverseLinkedList.ListNode node) {
-        while (node != null) {
-            System.out.print(node.val);
-            if (node.next != null) System.out.print("-->");
-            node = node.next;
-        }
-
-        System.out.println();
-    }
-
-    public static LeetCode206ReverseLinkedList.ListNode linkedListGiveValue(int[] nums) {
-        LeetCode206ReverseLinkedList.ListNode head = new LeetCode206ReverseLinkedList.ListNode(nums[0]);
-        LeetCode206ReverseLinkedList.ListNode a = head;
-
-        for (int i = 1; i < nums.length; i++) {
-            a.next = new LeetCode206ReverseLinkedList.ListNode(nums[i]);
-            a = a.next;
-        }
-
-        return head;
-    }
 }
