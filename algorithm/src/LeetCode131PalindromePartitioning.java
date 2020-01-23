7b0ca4291d67357cc1d88f6dcf35bbdcb7506e4b
lonely7yk <893077810@qq.com>
2020-01-22 16:13:49 -0800

    LeetCode131 DFS+DP

--- algorithm/src/LeetCode131PalindromePartitioning.java
+++ algorithm/src/LeetCode131PalindromePartitioning.java
@@ -48,37 +48,76 @@ public class LeetCode131PalindromePartitioning {
 //        return result;
 //    }
 
-    // 法二：backtrack-DFS 95%+
-    public List<List<String>> partition(String s) {
-        List<List<String>> result = new ArrayList<>();
-        List<String> currList = new ArrayList<>();
-        partition(s, 0, currList, result);
-        return result;
-    }
+    // // 法二：backtrack-DFS 95%+
+    // public List<List<String>> partition(String s) {
+    //     List<List<String>> result = new ArrayList<>();
+    //     List<String> currList = new ArrayList<>();
+    //     partition(s, 0, currList, result);
+    //     return result;
+    // }
 
-    private void partition(String s, int index, List<String> currList, List<List<String>> result) {
-        if (index == s.length()) {
-            result.add(new ArrayList<>(currList));
+    // private void partition(String s, int index, List<String> currList, List<List<String>> result) {
+    //     if (index == s.length()) {
+    //         result.add(new ArrayList<>(currList));
+    //         return;
+    //     }
+
+    //     for (int i = index; i < s.length(); i++) {
+    //         if (isPalindrome(s, index, i)) {
+    //             String currStr = s.substring(index, i + 1);
+    //             currList.add(currStr);
+    //             partition(s, i + 1, currList, result);
+    //             currList.remove(currList.size() - 1);
+    //         }
+    //     }
+    // }
+
+    // // 判断是否为回文
+    // private boolean isPalindrome(String s, int begin, int end) {
+    //     for (int i = begin; i <= (begin + end) / 2; i++) {
+    //         if (s.charAt(i) != s.charAt(end - (i - begin))) return false;
+    //     }
+
+    //     return true;
+    // }
+
+
+    // DP+DFS: 2ms 95%
+    public List<List<String>> partition(String s) {
+        if (s.length() == 0) return new ArrayList<>();
+        
+        boolean[][] dp = new boolean[s.length()][s.length()];
+        List<List<String>> res = new ArrayList<>();
+        List<String> curr = new ArrayList<>();
+        
+        // 这步 DP 是关键
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = 0; j <= i; j++) {
+                if (s.charAt(i) == s.charAt(j) && (i - j <= 2 || dp[j + 1][i - 1])) {
+                    dp[j][i] = true;
+                }
+            }
+        }
+        
+        dfs(curr, res, 0, s, dp);
+        
+        return res;
+    }
+    
+    private void dfs(List<String> curr, List<List<String>> res, int idx, String s, boolean[][] dp) {
+        if (idx == s.length()) {
+            res.add(new ArrayList<>(curr));
             return;
         }
-
-        for (int i = index; i < s.length(); i++) {
-            if (isPalindrome(s, index, i)) {
-                String currStr = s.substring(index, i + 1);
-                currList.add(currStr);
-                partition(s, i + 1, currList, result);
-                currList.remove(currList.size() - 1);
+        
+        for (int i = idx + 1; i < s.length() + 1; i++) {
+            // [idx, i)
+            if (dp[idx][i - 1]) {
+                String tmp = s.substring(idx, i);
+                curr.add(tmp);
+                dfs(curr, res, i, s, dp);
+                curr.remove(curr.size() - 1);
             }
         }
     }
-
-    // 判断是否为回文
-    private boolean isPalindrome(String s, int begin, int end) {
-        for (int i = begin; i <= (begin + end) / 2; i++) {
-            if (s.charAt(i) != s.charAt(end - (i - begin))) return false;
-        }
-
-        return true;
-    }
-
 }
