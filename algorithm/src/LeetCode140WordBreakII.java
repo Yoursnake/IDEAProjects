fb79c7404c8d9d5873472611d6fc9155075073e4
lonely7yk <893077810@qq.com>
2020-01-23 20:23:23 -0800

    LeetCode140 DFS+HashMap

--- algorithm/src/LeetCode140WordBreakII.java
+++ algorithm/src/LeetCode140WordBreakII.java
@@ -45,54 +45,84 @@ Output:
 []
  */
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class LeetCode140WordBreakII {
-    // DP + DFS 7.03%
+    // // DP + DFS 7.03%
+    // public List<String> wordBreak(String s, List<String> wordDict) {
+    //     List<String> result = new ArrayList<>();
+    //     String curr = "";
+    //     // 第 i 个表示前 i 个能到达的上一个索引 list，如果不能达到则为空
+    //     // 注：每个 list 存储的是索引
+    //     List<List<Integer>> trackList = new ArrayList<>();
+
+    //     // 第 0 个表示前 0 个字符串初始化为 -1 表明可以达到
+    //     trackList.add(new ArrayList<>(Collections.singletonList(-1)));
+
+    //     for (int i = 1; i <= s.length(); i++) {
+    //         List<Integer> track = new ArrayList<>();
+    //         for (int j = 0; j < i; j++) {
+    //             // 如果前 j 个字符串可以达到，且字典包含索引 j~i-1 的字符串，
+    //             // 则说明前 i 个也可以达到
+    //             if (trackList.get(j).size() > 0 && wordDict.contains(s.substring(j, i))) {
+    //                 track.add(j);
+    //             }
+    //         }
+    //         trackList.add(track);
+    //     }
+
+    //     DFS(trackList, result, s, s.length(), curr);
+    //     return result;
+    // }
+
+    // private void DFS(List<List<Integer>> trackList, List<String> result, String s, int index, String curr) {
+    //     // index 表示现在指向的 trackList 的位置
+    //     if (index == 0) {
+    //         result.add(curr);
+    //         return;
+    //     }
+
+    //     if (!curr.equals("")) curr = " " + curr;
+
+    //     // 到达 index 的前面的索引
+    //     List<Integer> beforeTracks = trackList.get(index);
+
+    //     for (int beforeTrack : beforeTracks) {
+    //         String temp = curr;
+    //         temp = s.substring(beforeTrack, index) + curr;
+    //         DFS(trackList, result, s, beforeTrack, temp);
+    //     }
+    // }
+
+    // DFS + HashMap: 31ms 5.26%
     public List<String> wordBreak(String s, List<String> wordDict) {
-        List<String> result = new ArrayList<>();
-        String curr = "";
-        // 第 i 个表示前 i 个能到达的上一个索引 list，如果不能达到则为空
-        // 注：每个 list 存储的是索引
-        List<List<Integer>> trackList = new ArrayList<>();
-
-        // 第 0 个表示前 0 个字符串初始化为 -1 表明可以达到
-        trackList.add(new ArrayList<>(Collections.singletonList(-1)));
-
+        Set<String> wordSet = new HashSet<>(wordDict);
+        Map<String, List<String>> map = new HashMap<>();
+        return dfs(s, wordSet, map);
+    }
+    
+    private List<String> dfs(String s, Set<String> wordSet, Map<String, List<String>> map) {
+        if (map.containsKey(s)) {
+            return map.get(s);
+        }
+        
+        List<String> res = new ArrayList<>();
         for (int i = 1; i <= s.length(); i++) {
-            List<Integer> track = new ArrayList<>();
-            for (int j = 0; j < i; j++) {
-                // 如果前 j 个字符串可以达到，且字典包含索引 j~i-1 的字符串，
-                // 则说明前 i 个也可以达到
-                if (trackList.get(j).size() > 0 && wordDict.contains(s.substring(j, i))) {
-                    track.add(j);
+            String curr = s.substring(0, i);
+            if (wordSet.contains(curr)) {
+                // 这句相当于边界条件
+                if (i == s.length()) {
+                    res.add(curr);
+                } else {
+                    List<String> afterStrs = dfs(s.substring(i), wordSet, map);
+                    for (String afterStr : afterStrs) {
+                        res.add(curr + " " + afterStr);
+                    }
                 }
             }
-            trackList.add(track);
         }
-
-        DFS(trackList, result, s, s.length(), curr);
-        return result;
-    }
-
-    private void DFS(List<List<Integer>> trackList, List<String> result, String s, int index, String curr) {
-        // index 表示现在指向的 trackList 的位置
-        if (index == 0) {
-            result.add(curr);
-            return;
-        }
-
-        if (!curr.equals("")) curr = " " + curr;
-
-        // 到达 index 的前面的索引
-        List<Integer> beforeTracks = trackList.get(index);
-
-        for (int beforeTrack : beforeTracks) {
-            String temp = curr;
-            temp = s.substring(beforeTrack, index) + curr;
-            DFS(trackList, result, s, beforeTrack, temp);
-        }
+        
+        map.put(s, res);
+        return res;
     }
 }
